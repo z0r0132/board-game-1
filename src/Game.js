224ca5038ee4f3f0887231abcadd8edd888f6743
Joseph El Alam <joseph.elalam@gmail.com>
2021-02-07 14:31:01 +0100

    implement end game condition winning and draw

--- src/Game.js
+++ src/Game.js
@@ -41,8 +41,61 @@ const turn = {
   moveLimit: 1,
 };
 
+/**
+ * IsDraw function check if all cells are filled
+ * which means that there is draw
+ * @param {array} cells of cells
+ * @returns {Boolean}  return true if all cells are filled
+ */
+const isDraw = (cells) => cells.filter((c) => c === null).length === 0;
+
+/**
+ * IsVictory function check if any completed state if filled
+ * @param {array} cells of cells
+ * @returns {Boolean}  return if there is a victory
+ */
+function IsVictory(cells) {
+  // winning rows, cols or diagonals
+  const positions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  // check if a complete (col, row, diagonal) is solved
+  // by checking if all cells has symbol similar to first and all are not null
+  const isRowComplete = (row) => {
+    const symbols = row.map((i) => cells[i]);
+    return symbols.every((i) => i !== null && i === symbols[0]);
+  };
+
+  // return tre if at least one isRowComplete check of a position in positions is true
+  return positions.map(isRowComplete).some((i) => i === true);
+}
+/**
+ * EndIf the game end conditions
+ * @param {Object} G
+ * @param {Object} ctx
+ * @returns {Object} null if not finished or Object describing draw or win
+ */
+const endIf = (G, ctx) => {
+  const cells = G.cells;
+  if (IsVictory(cells)) {
+    return { winner: ctx.currentPlayer };
+  }
+  if (IsDraw(cells)) {
+    return { draw: true };
+  }
+};
+
 export const TicTacToe = {
   setup,
   moves,
   turn,
+  endIf,
 };
